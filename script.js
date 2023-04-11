47f0af50107b0c8ee08cef9c1b73f86a95f6c0bc
gersonhiggins <gahg.10gerson@gmail.com>
2023-04-11 11:27:04 -0400

    Add JS menu dropdown

---
+++ script.js
@@ -0,0 +1,27 @@
+const hamburguer = document.querySelector('.hamburguer');
+const navMenu = document.querySelector('.nav-menu');
+const intro = document.querySelector('.introbox');
+const grid = document.querySelector('.grid-layout');
+const about = document.querySelector('.about');
+const contact = document.querySelector('.contact_form');
+const brand = document.querySelector('.nav-brand');
+
+hamburguer.addEventListener('click', () => {
+  hamburguer.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  intro.classList.toggle('blur');
+  grid.classList.toggle('blur');
+  about.classList.toggle('blur');
+  contact.classList.toggle('blur');
+  brand.classList.toggle('blur');
+});
+
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburguer.classList.remove('active');
+  navMenu.classList.remove('active');
+  intro.classList.remove('blur');
+  grid.classList.remove('blur');
+  about.classList.remove('blur');
+  contact.classList.remove('blur');
+  brand.classList.remove('blur');
+}));
